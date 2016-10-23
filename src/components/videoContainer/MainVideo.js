940036d4738fac04943ce8d09ab7db1acc62e926
Daniel Hoffens <dhoffens@gmail.com>
2016-10-23 11:47:35 -0400

    5 videos

--- src/components/videoContainer/MainVideo.js
+++ src/components/videoContainer/MainVideo.js
@@ -7,21 +7,36 @@ export default class MainVideo extends Component {
     if (!mainVideo){
       return <div>Loading...</div>
     }
-    const theVideo = mainVideo[0];
-    const videoId = theVideo.id.videoId;
-    const url = `http://www.youtube.com/embed/${videoId}`;
+
+    const videos = mainVideo.map(video=>{
+	    const videoId = video.id.videoId;
+	    const url = `http://www.youtube.com/embed/${videoId}`;
+
+    	return (
+	        <li>
+		        <div className="video-detail">
+
+		          <div className="embed-responsive embed-responsive-16by9">
+		            <iframe className="embed-responsive-item" src={url} />
+		          </div>
+
+		          <div className="details">
+		            <div>{video.snippet.title}</div>
+		          </div>
+		        </div>
+		    </li>
+    	)
+    })
+
+    // const theVideo = mainVideo[0];
+    // const videoId = theVideo.id.videoId;
+    // const url = `http://www.youtube.com/embed/${videoId}`;
     
     return (
-        <div className="video-detail">
 
-          <div className="embed-responsive embed-responsive-16by9">
-            <iframe className="embed-responsive-item" src={url} />
-          </div>
-
-          <div className="details">
-            <div>{theVideo.snippet.title}</div>
-          </div>
-        </div>
+    	<ul>
+	      {videos}
+	    </ul>
       )
     }
 }
